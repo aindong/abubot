2df01f742d90b9a0f80db493d51695f1adc386af
Alleo Indong <freedom_war550@live.com.ph>
2019-07-03 23:33:25 +0800

    added some emoji

--- commands/deployFront.js
+++ commands/deployFront.js
@@ -42,7 +42,7 @@ module.exports = async (slack, event, project, environment) => {
   child.on("close", code => {
     if (code === 0) {
       slack.chat.postMessage({
-        text: `Nadeploy ko na ang ${project} sa ${environment}. Maaari mo na itong subukan. Maraming salamat!`,
+        text: `:check: Nadeploy ko na ang ${project} sa ${environment}. Maaari mo na itong subukan. Maraming salamat! :tada: :tada:`,
         channel: event.channel
       });
     } else {
